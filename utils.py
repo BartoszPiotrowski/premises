7bccc554fcb40f81ea8d2e3b45c758fea9d8ad58
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2017-12-13 15:52:18 +0100

    Reading dict, better way.

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 
 def remove_supersets(list_of_sets):
-    """Removes proper supersets from the list of sets"""
+    '''Removes proper supersets from the list of sets'''
     list_of_sets_clean = []
     l = len(list_of_sets)
     for i1 in range(l):
@@ -10,3 +10,24 @@ def remove_supersets(list_of_sets):
         else:
             list_of_sets_clean.append(list_of_sets[i1])
     return list_of_sets_clean
+
+def read_dict(filename, type_of_names=str, type_of_values=str, sep=':',
+             sep_in_list=', ', to_strip=' "'):
+    with open(filename) as file:
+        slines = [l.split(sep) for l in file.read().splitlines()]
+    names = [type_of_names(l[0]) for l in slines]
+    values_raw = [l[1] for l in slines]
+    if type_of_values in {int, str, float}
+        values = [type_of_values(v.strip(to_strip)) for v in values_raw]
+    elif type_of_values == list:
+        values = []
+        for v in values_raw:
+            if v:
+                values.append([type_of_values_in_list(i.strip(to_strip))
+                                   for i in v.split(sep_in_list)])
+            else:
+                values.append([])
+    else:
+        print("Error: cannot read the file to dict.")
+        return
+    return dict(zip(names, values))
