3d7768284ff242de55eaa7caaac4d2207ebd5734
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2018-01-11 00:29:23 +0100

    Loop.

---
+++ tests/loop.py
@@ -0,0 +1,28 @@
+PACKAGE_DIR = '/home/bartosz/PhD'
+DATA_DIR = 'data/debug_data'
+ATP_DIR = 'atp'
+LOG_FILE = __file__.replace('.py', '.log')
+N_JOBS = 4
+
+
+import sys
+from os.path import join
+from random import sample
+sys.path.append(PACKAGE_DIR)
+import premises as prs
+
+statements = prs.Statements(from_file=join(DATA_DIR, 'statements'),
+                            logfile=LOG_FILE)
+features = prs.Features(from_file=join(DATA_DIR, 'features'), logfile=LOG_FILE)
+chronology = prs.Chronology(from_file=join(DATA_DIR, 'chronology'),
+                            logfile=LOG_FILE)
+print(set(statements))
+theorems = sample(set(statements), 20)
+params_rankings = {'chronology': chronology}
+
+# randomly generated rankings
+rankings_test = prs.Rankings(test_theorems, model=None, params=params_rankings,
+                             n_jobs=N_JOBS, logfile=LOG_FILE)
+proofs_test = prs.atp_evaluation(rankings_test, statements, dirpath=ATP_DIR,
+                                 n_jobs=N_JOBS, logfile=LOG_FILE)
+
