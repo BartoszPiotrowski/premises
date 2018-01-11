7f28f480c7b252a50d754d0837251f95fd583e98
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2018-01-11 14:15:32 +0100

    Loop test.

--- tests/loop.py
+++ tests/loop.py
@@ -16,13 +16,32 @@ statements = prs.Statements(from_file=join(DATA_DIR, 'statements'),
 features = prs.Features(from_file=join(DATA_DIR, 'features'), logfile=LOG_FILE)
 chronology = prs.Chronology(from_file=join(DATA_DIR, 'chronology'),
                             logfile=LOG_FILE)
-print(set(statements))
-theorems = sample(set(statements), 20)
-params_rankings = {'chronology': chronology}
+# TODO separate theorems and definitions
+theorems = sample(prs.utils.readlines(join(DATA_DIR, 'theorems')), 5)
+print(theorems)
+params = {'features': features,
+         'features_ordered': features.all_features(),
+         'chronology': chronology,
+         'sparse': False}
 
 # randomly generated rankings
-rankings_test = prs.Rankings(test_theorems, model=None, params=params_rankings,
+rankings_random = prs.Rankings(theorems, model=None, params=params,
                              n_jobs=N_JOBS, logfile=LOG_FILE)
-proofs_test = prs.atp_evaluation(rankings_test, statements, dirpath=ATP_DIR,
+
+proofs = prs.atp_evaluation(rankings_random, statements, dirpath=ATP_DIR,
                                  n_jobs=N_JOBS, logfile=LOG_FILE)
 
+while True:
+    train_labels, train_array = prs.proofs_to_train(proofs, params,
+                                           n_jobs=N_JOBS, logfile=LOG_FILE)
+
+    params_train = {}
+    model = prs.train(train_labels, train_array, params=params_train,
+                        n_jobs=N_JOBS, logfile=LOG_FILE)
+
+    rankings = prs.Rankings(theorems, model, params_data_trans,
+                         n_jobs=N_JOBS, logfile=LOG_FILE)
+
+    params_atp_eval = {}
+    proofs = prs.atp_evaluation(rankings, statements, params_atp_eval,
+                             dirpath=ATP_DIR, n_jobs=N_JOBS, logfile=LOG_FILE)
