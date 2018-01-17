1f3e315a0fa290431f5aa72cf57856909b6d192f
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2018-01-17 14:02:58 +0100

    Other boosters in xgboost available.

--- training.py
+++ training.py
@@ -19,14 +19,20 @@ def train(labels, array, weights=None, model="xgboost", params={}, n_jobs=-1,
     return trained_model
 
 def train_xgboost(labels, array, weights, params, n_jobs):
-    num_boost_round = params["num_boost_round"] \
-            if "num_boost_round" in params else 100
-    eta = params["eta"] if "eta" in params else 0.05
-    max_depth = params["max_depth"] if "max_depth" in params else 10
-    xgb_pretrained_model = params["xgb_pretrained_model"] \
-            if "xgb_pretrained_model" in params else None
+    num_boost_round = params['num_boost_round'] \
+            if 'num_boost_round' in params else 100
+    eta = params['eta'] if 'eta' in params else 0.05
+    max_depth = params['max_depth'] if 'max_depth' in params else 10
+    booster = params['booster'] if 'booster' in params else 'gbtree'
+    # 'gblinear' and 'dart' also available;
+    # 'gblinear' makes xgboost like lasso
+    xgb_pretrained_model = params['xgb_pretrained_model'] \
+            if 'xgb_pretrained_model' in params else None
     dtrain = xgb.DMatrix(array, label=labels, weight=weights)
-    params_booster = {'eta': eta, 'max_depth': max_depth,
-                      'objective': 'binary:logistic', 'n_jobs': n_jobs}
+    params_booster = {'eta': eta,
+                      'max_depth': max_depth,
+                      'objective': 'binary:logistic',
+                      'booster': booster,
+                      'n_jobs': n_jobs}
     return xgb.train(params_booster, dtrain, num_boost_round=num_boost_round,
                      xgb_model=xgb_pretrained_model)
