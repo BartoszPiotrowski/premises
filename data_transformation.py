cacdf6e7de196ec6c7cbb5c4232639c9a92a7d7e
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2018-01-19 19:58:27 +0100

    Cleaning.

--- data_transformation.py
+++ data_transformation.py
@@ -8,39 +8,8 @@ from sklearn.feature_extraction import FeatureHasher
 from time import time
 from .utils import printline
 
-
-def bin_vector(list_of_features, order_of_features):
-#   return [int(i in list_of_features) for i in order_of_features]
-# is it OK to have bools instead of ints?
-    return [i in list_of_features for i in order_of_features]
-
-def bin_trans_comb(thm_features, prm_features, order_of_features):
-  #  vector = []
-  #  for f in order_of_features:
-  #      T = f in thm_features
-  #      P = f in prm_features
-  #      if T or P:
-  #          if T and P:
-  #              vector.append(1)
-  #          else:
-  #              vector.append(-1)
-  #      else:
-  #          vector.append(0)
-  #  t0 = time()
-    vector = [0 if not (f in thm_features or f in prm_features) else \
-              (1 if f in thm_features and f in prm_features else -1)
-                      for f in order_of_features]
-  #  t1 = time(); print("1", t1 - t0)
-    return vector
-
-def bin_trans_concat(thm_features, prm_features, order_of_features):
-    vector_thm = bin_vector(thm_features, order_of_features)
-    vector_prm = bin_vector(prm_features, order_of_features)
-    return vector_thm + vector_prm
-
 # pair means here (thm features, prm features)
 def pairs_to_array(pairs, params):
-#    order_of_features = params['features'].order_of_features
   num_of_features = params['features'].num_of_features
   list_of_pairs = [list(thm_f) + list(prm_f) for thm_f, prm_f in pairs]
   hasher = FeatureHasher(n_features=num_of_features, input_type='string')
@@ -84,7 +53,6 @@ def proofs_to_train_one_theorem(thm, atp_useful, params):
     labels = [1] * len(pairs_pos) + [0] * len(pairs_neg)
     array = pairs_to_array(pairs_pos + pairs_neg, params)
     assert len(labels) == array.shape[0]
-    print(thm)
     return labels, array
 
 def proofs_to_train(proofs, params, n_jobs=-1, verbose=True, logfile=''):
