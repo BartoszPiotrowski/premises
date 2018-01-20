fadaab61e26ea50603f1683289d5424ae8815a33
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2018-01-20 23:37:01 +0100

    Possible to set number of features and number of thms for neg. min.

--- data_transformation.py
+++ data_transformation.py
@@ -10,17 +10,19 @@ from .utils import printline, partition
 
 # pair means here (thm features, prm features)
 def pairs_to_array(pairs, params):
-    num_of_features = params['features'].num_of_features
+    num_of_features = params['num_of_features']
     merge_mode = params['merge_mode']
     if merge_mode == 'comb':
         list_of_pairs = [list(thm_f) + list(prm_f) for thm_f, prm_f in pairs]
-    else:
+    elif merge_mode == 'concat':
         num_of_features = 2 * num_of_features
         list_of_pairs = []
         for thm_f, prm_f in pairs:
             thm_f_appended = ['T' + f for f in thm_f]
             prm_f_appended = ['T' + f for f in prm_f]
             list_of_pairs.append(thm_f_appended + prm_f_appended)
+    else:
+        print("Error: unknown merge mode.")
     hasher = FeatureHasher(n_features=num_of_features, input_type='string')
     csc_array = hasher.transform(list_of_pairs)
     return csc_array
@@ -38,13 +40,8 @@ def proofs_to_train_n_thms(thms, proofs, params):
 def thm_to_labels_and_pairs(thm, atp_useful, params):
     features = params['features']
     chronology = params['chronology']
+    ratio_neg_pos = params['ratio_neg_pos']
     available_premises = chronology.available_premises(thm)
-    ratio_neg_pos = params['ratio_neg_pos'] \
-        if 'ratio_neg_pos' in params else 5
-    rankings_for_neg_mining = params['rankings_for_negative_mining'] \
-        if 'rankings_for_negative_mining' in params else None
-    level_of_neg_mining = params['level_of_negative_mining'] \
-        if 'level_of_negative_mining' in params else 2
     not_pos_premises = set(available_premises) - set(atp_useful)
     # TODO something more clever; differentiate importance of positives
     pos_premises = atp_useful
@@ -52,9 +49,11 @@ def thm_to_labels_and_pairs(thm, atp_useful, params):
         labels = [1] * len(pos_premises)
         pairs = [(features[thm], features[prm]) for prm in pos_premises]
         return labels, pairs
-    if rankings_for_neg_mining:
+    if thm in params['thms_for_negative_mining']:
+        level_of_negative_mining = params['level_of_negative_mining']
+        ranking_thm = params['rankings_for_negative_mining'][thm]
         neg_premises_misclass = misclassified_negatives(
-            rankings_for_neg_mining[thm], atp_useful, level_of_neg_mining)
+            ranking_thm, atp_useful, level_of_negative_mining)
         neg_premises_not_misclass = not_pos_premises - neg_premises_misclass
         num_neg_premises_not_misclass = \
             min(len(neg_premises_not_misclass), ratio_neg_pos * len(pos_premises))
@@ -71,20 +70,53 @@ def thm_to_labels_and_pairs(thm, atp_useful, params):
     return labels, pairs
 
 def proofs_to_train(proofs, params, n_jobs=-1, verbose=True, logfile=''):
+    # checking and initializing params
     assert len(proofs) > 0
     assert 'features' in params
     assert 'chronology' in params
     if not 'merge_mode' in params:
         params['merge_mode'] = 'concat'
-    if 'rankings_for_negative_mining' in params:
+    if not 'ratio_neg_pos' in params:
+        params['ratio_neg_pos'] = 5
+    if not 'num_of_features' in params:
+        params['num_of_features'] = 1
+    assert params['num_of_features'] > 0
+    if params['num_of_features'] <= 1:
+        params['num_of_features'] = \
+        int(params['num_of_features'] * params['features'].num_of_features)
+    else:
+        params['num_of_features'] = \
+        min(params['num_of_features'], params['features'].num_of_features)
+    if not 'rankings_for_negative_mining' in params:
+        params['thms_for_negative_mining'] = {}
+    else:
         assert set(params['rankings_for_negative_mining']) >= set(proofs)
+        if not 'level_of_negative_mining' in params:
+            params['level_of_negative_mining'] = 2
+        if not 'part_for_negative_mining' in params:
+            params['part_for_negative_mining'] = 0.2
+        params['thms_for_negative_mining'] = sample(set(proofs),
+                    int(len(proofs) * params['part_for_negative_mining']))
+    # printing informations
     if verbose or logfile:
-        printline("Transforming proofs into training data...", logfile, verbose)
-        printline("    Negative mining: {}".format(
-                'rankings_for_negatve_mining' in params), logfile, verbose)
-        printline(("    Mode of combining thms and premises to "
-                   "examples: merge_mode={}".format(params['merge_mode'])),
-                  logfile, verbose)
+        printline("Transforming proofs of {} theorems to training data...".format(
+                      len(proofs)), logfile, verbose)
+        num_of_all_features = params['features'].num_of_features
+        printline("    Number of features used: {} / {}".format(
+                params['num_of_features'], params['features'].num_of_features),
+                        logfile, verbose)
+        printline(("    Mode of combining theorems and premises to examples: "
+               "merge_mode={}".format(params['merge_mode'])), logfile, verbose)
+        printline("    Negatives to positive ratio: {}".format(
+                params['ratio_neg_pos']), logfile, verbose)
+        if params['thms_for_negative_mining']:
+            printline("    Negative mining:", logfile, verbose)
+            printline("        Level of negative mining: {}".format(
+                    params['level_of_negative_mining']), logfile, verbose)
+            printline("        Part of theorems for negative mining: {}".format(
+                    params['part_for_negative_mining']), logfile, verbose)
+        else:
+            printline("    No negative mining.", logfile, verbose)
     all_proved_thms = list(proofs)
     thms_splited = partition(all_proved_thms, max(n_jobs, 4))
     with Parallel(n_jobs=n_jobs) as parallel:
@@ -101,18 +133,18 @@ def proofs_to_train(proofs, params, n_jobs=-1, verbose=True, logfile=''):
     return labels, array
 
 # returns the most misclassified negatives
-def misclassified_negatives(ranking, atp_useful, level_of_neg_mining=2):
-    if isinstance(level_of_neg_mining, int):
+def misclassified_negatives(ranking, atp_useful, level_of_negative_mining=2):
+    if isinstance(level_of_negative_mining, int):
         n_pos = len(atp_useful)
-        n_neg = int(n_pos * level_of_neg_mining)
+        n_neg = int(n_pos * level_of_negative_mining)
         mis_negs = [ranking[i] for i in range(min(n_neg, len(ranking)))
                     if not ranking[i] in set(atp_useful)]
-    elif level_of_neg_mining == 'all':
+    elif level_of_negative_mining == 'all':
         max_pos = max([i if prm in atp_useful else 0
                     for i, prm in enumerate(ranking)])
         mis_negs = [ranking[i] for i in range(min(max_pos, len(ranking)))
                     if not ranking[i] in set(atp_useful)]
-    elif level_of_neg_mining == 'random':
+    elif level_of_negative_mining == 'random':
         max_pos = max([i if prm in atp_useful else 0
                     for i, prm in enumerate(ranking)])
         mis_negs_all = [ranking[i] for i in range(min(max_pos, len(ranking)))
--- tests/negative_mining.py
+++ tests/negative_mining.py
@@ -16,22 +16,22 @@ chronology = prs.Chronology(from_file=join(DATA_DIR, 'chronology'),
                             logfile=LOG_FILE)
 proofs_train = prs.Proofs(from_file=join(DATA_DIR, 'atpproved.train'),
                           logfile=LOG_FILE)
-proofs_test = prs.Proofs(from_file=join(DATA_DIR, 'atpproved.test'),
-                          verbose=False)
+theorems = prs.utils.readlines(join(DATA_DIR, 'theorems_atpproved'))
 train_theorems = set(proofs_train)
-test_theorems = set(proofs_test)
+test_theorems = set(theorems) - set(train_theorems)
 params_data_trans = {'features': features,
                      'chronology': chronology,
-                     'merge_mode': 'concat',
-                     'sparse': False}
+                     'num_of_features': 0.1,
+                     'merge_mode': 'concat'}
 train_labels, train_array = prs.proofs_to_train(proofs_train, params_data_trans,
                                                n_jobs=N_JOBS, logfile=LOG_FILE)
-params_train = {'booster': 'gblinear'}
+params_train = {'num_boost_round': 10}
 model = prs.train(train_labels, train_array, params=params_train,
                     n_jobs=N_JOBS, logfile=LOG_FILE)
 rankings_train = prs.Rankings(train_theorems, model, params_data_trans,
                      n_jobs=N_JOBS, logfile=LOG_FILE)
 params_data_trans['rankings_for_negative_mining'] = rankings_train
+params_data_trans['level_of_negative_mining'] = 'all'
 train_labels, train_array = prs.proofs_to_train(proofs_train, params_data_trans,
                        n_jobs=N_JOBS, logfile=LOG_FILE)
 model = prs.train(train_labels, train_array, params=params_train,
