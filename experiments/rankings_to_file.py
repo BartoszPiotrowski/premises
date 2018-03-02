0a2bc430ee9e392f8419abbdccd48e69e57b87f6
Mgr. Bartosz Pawel Piotrowski <piotrbar@grid-04.cluster>
2018-03-02 14:43:50 +0100

    Fix.

--- experiments/rankings_to_file.py
+++ experiments/rankings_to_file.py
@@ -8,9 +8,10 @@ N_JOBS = 10
 DATA_DIR = 'data/MPTP2078'
 ATP_DIR = 'atp'
 RANKINGS_DIR = 'rankings'
+RANKINGS_FILES_DIR = 'rankings_1'
 LOG_FILE = __file__.replace('.py', '.log')
 
-rankings = prs.utils.load_obj(join(RANKINGS_DIR, rankings_multi_mizar40_1.pkl))
+rankings = prs.utils.load_obj(join(RANKINGS_DIR, 'rankings_multi_mizar40_1.pkl'))
 for r in rankings:
     print(r)
-    prs.utils.writelines(rankings[r], r)
+    prs.utils.writelines(rankings[r], join(RANKINGS_FILES_DIR, r))
